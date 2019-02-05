57e5b5cb13c6de973d58cc2f938c87ffa11a25d3
F376338 <michelin18>
2019-02-05 17:17:42 +0100

    Added UC1-3

---
+++ src/main/java/fr/isima/yk/container/AuditService.java
@@ -0,0 +1,4 @@
+package fr.isima.yk.container;
+
+public abstract class AuditService {
+}
---
+++ src/main/java/fr/isima/yk/container/WebMovieFinder.java
@@ -0,0 +1,7 @@
+package fr.isima.yk.container;
+
+public class WebMovieFinder extends MovieFinder {
+    public void doAct() {
+        System.out.println("MovieFinder");
+    }
+}
